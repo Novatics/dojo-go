9abc3c5c2d6af4af602b3648c405040db434472c
Alvaro Sobral de Oliveira <alvaro.sobral.oliveira@gmail.com>
2022-12-15 17:44:06 -0300

    feat: alvaro

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+type register struct {
+	nota100 int
+	nota50 int
+	nota20 int
+	nota10 int
+}
+
 func main() {
 
 	fmt.Print("Digite o valor \n")
@@ -9,12 +16,21 @@ func main() {
 
 	// Taking input from user
 	fmt.Scanln(&saqueValor)
+	// s := register{nota100: 100, nota50: 100, nota20: 100, nota10: 100}
 
-	calculaSaque(&saqueValor)
+	calculaSaque(100)
 	fmt.Print(saqueValor)
 
 }
 
-func calculaSaque(int &valor) {
-	valor = valor / 2
+func calculaSaque(valor int) int {
+	// nota100 := valor / 100
+
+
+	// nota50 := valor / 50
+
+	// nota20 := valor / 20
+
+	// nota10 := valor /10
+	return valor
 }
--- main_test.go
+++ main_test.go
@@ -1,10 +1,14 @@
 package main
 
-
 import (
 	"testing"
 )
 
 func testSaqueWithNegativeValueShouldThrowException(t *testing.T) {
-	got :=  
+	got :=  calculaSaque(100)
+	want := 0
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
 }
